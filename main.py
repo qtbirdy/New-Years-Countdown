d2d4f3b5f157da2eda9c05ed2148e2ccec10e66a
Jacob <Jacob.e.charbonneau@gmail.com>
2022-02-27 16:08:32 -0500

    practise compplete!

--- main.py
+++ main.py
@@ -156,9 +156,15 @@
 #     time.sleep(1)
 # print('happy new year!')
 
+import time
 
+for seconds in range (10,0,-1):
+    print(seconds)
+    time.sleep(1)
+print("Happy new year!")
 
-print("hi")
+    
+
 
 
 
